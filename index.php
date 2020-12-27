0b4bd0d49239ec11a8a3479b7960580d29d7ea93
criss02-cs <cristianoaloigi0@gmail.com>
2020-12-27 18:07:16 +0100

    Aggiunta la select per visualizzare le varie classi dell'indirizzo

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    <form>
-        <select name="classi" id="classi">
-            
-        </select>
-    </form>
-</body>
+    <?php
+        include_once "mysql_configuration/connection.php";
+        $conn = $GLOBALS['mysqli'];
+        $sql = "SELECT * FROM classi";
+        $result = $conn->query($sql);
+    ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+    </head>
+    <body>
+
+        <form method="get" action="index.php" onchange="this.submit()">
+            <select name="classi" id="classi">
+                <option value="empty" selected> </option>
+                <?php 
+                    while($row = mysqli_fetch_assoc($result)){
+                        ?>
+                        <option value="<?php echo $row['Anno'].' '.$row['Sezione']?>"><?php echo $row['Anno'].' '.$row['Sezione'].' '.$row['AnnoScolastico']?></option>
+                    <?php }
+                ?>
+            </select>
+        </form>
+    </body>
 </html>
