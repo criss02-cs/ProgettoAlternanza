242e01d6659db409022530284f2df6fbb4a935e0
criss02-cs <cristianoaloigi0@gmail.com>
2020-12-29 23:53:33 +0100

    Aggiunta la scelta del periodo dell'alternanza dopo la scelta della classe

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
         $sql = "SELECT * FROM classi WHERE AnnoScolastico LIKE '". date("Y") ."%' 
                 OR AnnoScolastico LIKE '%". date("Y", strtotime("+1 years")) ."%'";
         $result = $conn->query($sql);
+        $classe = "";
+        if(!empty($_GET) && !empty($_GET['classi']))
+            $classe = $_GET['classi'];
     ?>
     <head>
         <meta charset="UTF-8">
@@ -14,17 +17,24 @@
         <title>Home</title>
     </head>
     <body>
-
         <form method="get" action="index.php" onchange="this.submit()">
             <select name="classi" id="classi">
-                <option value="empty" selected> </option>
+                <option value="empty" <?php if(empty($_GET['classi'])) echo 'selected';?>> </option>
                 <?php 
                     while($row = mysqli_fetch_assoc($result)){
                         ?>
-                        <option value="<?php echo $row['Anno'].' '.$row['Sezione']?>"><?php echo $row['Anno'].' '.$row['Sezione'].' '.$row['AnnoScolastico']?></option>
+                        <option value="<?php echo $row['Anno'].' '.$row['Sezione'].' '.$row['AnnoScolastico']?>" 
+                            <?php if(!empty($_GET['classi']) && $row['Anno'].' '.$row['Sezione'].' '.$row['AnnoScolastico'] == $classe) echo 'selected';?>>
+                                <?php echo $row['Anno'].' '.$row['Sezione'].' '.$row['AnnoScolastico']?>
+                        </option>
                     <?php }
                 ?>
             </select>
+            <?php
+                if(!empty($_GET) && !empty($_GET['classi']) && $_GET['classi'] != "empty"){
+                    include("periodo.php");
+                }
+            ?>
         </form>
     </body>
 </html>
---
+++ periodo.php
@@ -0,0 +1,4 @@
+<h3>Inizio periodo</h3><br>
+<input type="date" name="inizio" value="<?php if(!empty($_GET['inizio'])) echo $_GET['inizio'];?>">
+<h3>Fine periodo</h3><br>
+<input type="date" name="fine" value="<?php if(!empty($_GET['fine'])) echo $_GET['fine'];?>">
