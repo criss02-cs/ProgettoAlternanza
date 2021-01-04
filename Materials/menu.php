40ed74d7f74a8257f57534c7f450056a3884b943
criss02-cs <cristianoaloigi0@gmail.com>
2021-01-04 17:39:09 +0100

    Aggiunte alla navigation bar i link per gli ultimi inserimenti

--- Materials/menu.php
+++ Materials/menu.php
@@ -3,4 +3,6 @@
   <a <?php if(strpos($_SERVER['PHP_SELF'], 'Storico') !== false) echo 'class="active"'?> href="../StoricoStage/index.php">Storico Stage</a>
   <a <?php if(strpos($_SERVER['PHP_SELF'], 'inserimentoStage') !== false) echo 'class="active"'?> href="../inserimentoStage/index.php">Organizza Stage</a>
   <a <?php if(strpos($_SERVER['PHP_SELF'], 'inserimentoStudenti') !== false) echo 'class="active"'?> href="../inserimenti/inserimentoStudenti.html">Inserisci Studenti</a>
+  <a <?php if(strpos($_SERVER['PHP_SELF'], 'inserimentoAziende') !== false) echo 'class="active"'?> href="../inserimenti/inserimentoAziende.html">Inserisci Aziende</a>
+  <a <?php if(strpos($_SERVER['PHP_SELF'], 'inserimentoTutorScolastici') !== false) echo 'class="active"'?> href="../inserimenti/inserimentoTutorScolastici.html">Inserisci Tutor Scolastici</a>
 </div> 
