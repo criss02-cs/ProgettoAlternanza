de98f9447f0ff450f8e547db61c89b84974fd576
criss02-cs <cristianoaloigi0@gmail.com>
2021-01-22 10:54:26 +0100

    s

--- StampePdf/studenti.php
+++ StampePdf/studenti.php
@@ -8,15 +8,15 @@
         $pdf = new FPDF('P','mm','A4');
         $pdf->AddPage();
         $pdf->SetFont('Arial', 'B', 25);
-        $pdf->Write(10, "Lista tutor scolastici");
+        $pdf->Write(10, "Lista studenti");
         $pdf->Ln();
         $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(50,12,"Id Tutor Scolastico",1);
+        $pdf->Cell(50,12,"Matricola",1);
         $pdf->Cell(50,12,"Nome",1);
         $pdf->Cell(50,12,"Cognome",1);
         while($row = mysqli_fetch_assoc($result)){
             $pdf->Ln();
-            $pdf->Cell(50,12,$row['id_tutorscolastico'],1);
+            $pdf->Cell(50,12,$row['Matricola'],1);
             $pdf->Cell(50,12,$row['Nome'],1);
             $pdf->Cell(50,12,$row['Cognome'],1);
         }
