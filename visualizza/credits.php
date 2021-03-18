67371cba48b5cf6a8cdc8696da0793386ec45992
Lubitok <61598801+Lubitok@users.noreply.github.com>
2021-03-18 20:59:51 +0100

    Add files via upload

--- visualizza/credits.php
+++ visualizza/credits.php
@@ -9,8 +9,26 @@
     <body>
         <?php include("../Materials/menu.php"); ?>
         <div class="container">
-          <h3>  Developed by Aloigi Cristiano & Kuci Rubin
-		<?php echo date("d-m-Y");?></h3>
+          <div class="credits-title"><h2>Chi siamo?</h2></div>
+          <span class="credits-text">
+                Siamo due ragazzi del 5D Informatico dell'anno scolastico 2020-2021, Kuci Rubin e Aloigi Cristiano. <br><br>
+                I nostri professori ci hanno chiesto di realizzare un gestionale per la gestione degli studenti in alternanza scuola lavoro, allora abbiamo progettato il database ed il sito che state visitando ora con l'uso di HTML, CSS e PHP.
+          </span><br><br><br><br><br>
+          <div class="credits-images">
+                <div class="credits-image1">
+                    <img src="images/rubin-kuci.jpeg" class="credits-image"><br>
+                    <span class="credits-text">Kuci Rubin</span>
+                </div>
+                <div class="credits-image2">
+                    <img src="images/cristiano-aloigi.png" class="credits-image"><br>
+                    <span class="credits-text">Aloigi Cristiano</span><br><br><br><br><br>
+                </div>
+          </div>
+          <div class="credits-title"><h2>Contattaci!</h2></div>
+          <span class="credits-text">
+                kuci.rubin@franchettisalviani.net <br>
+                aloigi.cristiano@franchettisalviani.net
+          </span><br><br><br><br><br>
         </div>
         <?php include("../Materials/footer.php"); ?>
     </body>
